10c17da81a74099b4727cde693208089dcb3a52a
gonzales caucota robin <rembertogonzales77@gmail.com>
2021-08-10 13:24:02 -0400

    version 1.0 inicializacion de rutas

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,30 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/* primera ruta la principal */
 Route::get('/', function () {
-    return view('welcome');
+   /* return view('welcome');*/
+   return "Bienvenido a la pagina principal";
+});
+
+/* segunda ruta */
+Route::get('cursos' , function () {
+    return "Bienvenido a la pagina cursos";
+});
+
+/* tercera ruta */
+Route::get('cursos/create', function () {
+    return "En esta pagina podras crear un curso";
+});
+
+/* cuarta ruta: mandando una variable por la url llamada curso ala ruta cursos*/
+Route::get('cursos/{curso}' , function ($curso) {
+    return "Bienvenido al curso: $curso";
+});
+
+/*quinta ruta:podemos pasar mas de una variable a una url */
+Route::get('cursos/{curso}/{categoria}', function ($curso,$categoria) {
+    return"Bienvenido al curso $curso, de la categoria $categoria";
 });
 
 Route::get('/dashboard', function () {
